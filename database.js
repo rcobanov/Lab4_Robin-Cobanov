756f486d479c85dcfcccfde739ef31bf216a2c64
robin <robin.cobanov@hotmail.com>
2023-03-08 14:18:10 +0100

    Modify database for lab4

---
+++ database.js
@@ -0,0 +1,52 @@
+const sqlite3 = require('sqlite3').verbose();
+
+const db = new sqlite3.Database('./users.db', sqlite3.OPEN_READWRITE, (err) => {
+  if (err) {
+    console.error(err.message);
+  }
+  console.log('Database created.')
+});
+
+function initDb() {
+  db.serialize(function () {
+    db.run("DROP TABLE IF EXISTS users;");
+    db.run("CREATE TABLE IF NOT EXISTS users(username TEXT NOT NULL PRIMARY KEY, name TEXT, role TEXT CHECK(role in('STUDENT', 'TEACHER','ADMIN')), password TEXT NOT NULL);");
+  })
+}
+
+async function registerUser(username, name, role, password) {
+  let checkUserExist = await getUser(username);
+  if (checkUserExist) {
+    return new Promise((resolve, reject) => {
+      reject(`User with username: "${username}" already exists.`)
+    })
+  }
+
+  return new Promise((resolve, reject) => {
+    db.run('INSERT INTO users(username, name, role, password) VALUES (?, ?, ?, ?);', [username, name, role, password], (err, res) =>{
+      if (err) {
+        reject(err);
+      } else {
+        resolve(res);
+      }
+    });
+  })
+}
+
+function getUser(username) {
+  return new Promise((resolve, reject) => {
+    db.get("SELECT * FROM users where username = ?",[username], (err, res) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(res);
+      }
+    });
+  });
+}
+
+module.exports = {
+  initDb: initDb,
+  registerUser: registerUser,
+  getUser, getUser,
+}
--- server.js
+++ server.js
@@ -10,6 +10,33 @@ require("dotenv").config();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+db.initDb();
+
+//Create default users
+createDefaultUsers('id1','user1', 'STUDENT', 'password');
+createDefaultUsers('id2','user2', 'STUDENT', 'password2');
+createDefaultUsers('id3', 'user3', 'TEACHER', 'password3');
+createDefaultUsers('admin', 'admin', 'ADMIN', 'admin');
+
+
+async function createDefaultUsers(username, name, role, password) {
+    let encryptedPassword = await bcrypt.hash(password, 10);
+    await db.registerUser(username, name, role, encryptedPassword);
+}
+
+
+//db.registerUser('user2', await bcrypt.hash('password2', 10));
+//db.registerUser('user3', await bcrypt.hash('password3', 10));
+//db.registerUser('admin', await(bcrypt.hash('admin'), 10));
+
+app.get('/', (req, res) => {
+  res.redirect('/identify')
+})
+
+app.get('/identify', (req, res) => {
+  res.render('identify.ejs')
+})
+
 app.listen(process.env.PORT, () => {
   console.log("App is listening on " + process.env.PORT + "...")
 })
