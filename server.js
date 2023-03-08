62bc6171ba19c98d74629b11c67ee03b881e9ecb
robin <robin.cobanov@hotmail.com>
2023-03-08 14:44:47 +0100

    Added register, login and jwt functionality

--- server.js
+++ server.js
@@ -2,13 +2,15 @@ const express = require('express');
 const app = express();
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
-const db = require('./database')
+const db = require('./database');
+const cookieParser = require('cookie-parser');
 app.set('view-engine', 'ejs');
 
 require("dotenv").config();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+app.use(cookieParser());
 
 db.initDb();
 
@@ -24,10 +26,11 @@ async function createDefaultUsers(username, name, role, password) {
     await db.registerUser(username, name, role, encryptedPassword);
 }
 
+//Middleware 
 
-//db.registerUser('user2', await bcrypt.hash('password2', 10));
-//db.registerUser('user3', await bcrypt.hash('password3', 10));
-//db.registerUser('admin', await(bcrypt.hash('admin'), 10));
+
+
+//Routes
 
 app.get('/', (req, res) => {
   res.redirect('/identify')
@@ -36,6 +39,68 @@ app.get('/', (req, res) => {
 app.get('/identify', (req, res) => {
   res.render('identify.ejs')
 })
+
+
+app.post('/identify', async (req, res) => {
+
+  //Fetch user from database
+  let dbUser = await db.getUser(req.body.username)
+  
+  //Render the fail page and return if user is not found
+  if (typeof dbUser === 'undefined') {
+    res.status(400).render('fail.ejs', { message: `The username "${req.body.username}" was not found.` });
+    return;
+  }
+  
+  //Make sure both fields are filled in 
+  if (req.body.password === '' || req.body.username === '' ) {
+    res.status(400).render('fail.ejs', { message: "You need to enter username and password" });
+    return;
+  }
+  
+  //User found and both fields are filled in
+  try {
+    if (await bcrypt.compare(req.body.password, dbUser.password)) {
+      //passwords match
+      let userObj = { username: req.body.username };
+      const token = jwt.sign(userObj, process.env.TOKENKEY)
+      console.log(token);
+      //send to user
+      res.cookie('jwt', token, { httpOnly: true });
+      res.render('start.ejs')
+    } else {
+      //passwords does not match
+      const message = `Incorrect password for user "${req.body.username}".`
+      res.status(401).render('fail.ejs', { message });
+    }
+  } catch(error){
+    console.log(error);
+  }
+})
+
+
+app.get('/REGISTER', (req, res) => {
+  res.render('register.ejs')
+})
+
+app.post('/REGISTER', async (req, res) => {
+  //If username or password is blank, render fail and return
+  if (req.body.password === '' || req.body.username === '') {
+    res.status(400).render('fail.ejs', { message: "You need to enter username and password" });
+    return;
+  }
+  
+  //if username and passwords are filled in:
+  try {
+    let encryptedPassword = await bcrypt.hash(req.body.password, 10);
+    await db.registerUser(req.body.username, req.body.name, req.body.role, encryptedPassword);
+    res.status(200).redirect('/identify')
+  } catch (error) {
+    res.status(400).render('fail.ejs', { message: error })
+    return;
+  }
+})
+
 
 app.listen(process.env.PORT, () => {
   console.log("App is listening on " + process.env.PORT + "...")
